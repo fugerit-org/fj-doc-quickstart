b0c15310a260798e1837e07dfc9932304a70c8f1
Matteo a.k.a. Fugerit <m@fugerit.org>
2021-03-20 21:26:28 +0100

    Quarkus demo app
    localhost:8080/pdftest
    
    Run on java 11

---
+++ fj-doc-quickstart-demo-quarkus/fj-doc-quickstart-demo-quarkus/src/test/java/org/fugerit/java/fjdocquickstartdemoquarkus/NativeFjDocQuickstartDemoQuarkusAppIT.java
@@ -0,0 +1,9 @@
+package org.fugerit.java.fjdocquickstartdemoquarkus;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeFjDocQuickstartDemoQuarkusAppIT extends FjDocQuickstartDemoQuarkusAppTest {
+
+    // Execute the same tests but in native mode.
+}
