9b45e884d0d5d5401f747927030eb79e0c13b732
Daneel <d@fugerit.org>
2022-10-28 09:31:58 +0200

    Modifica versione base

---
+++ fj-doc-quickstart-b-freemarker/src/main/resources/free_marker_quickstart/simple-markdown.ftl
@@ -0,0 +1,21 @@
+
+
+<doc
+	xmlns="http://javacoredoc.fugerit.org"
+	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
+    xsi:schemaLocation="http://javacoredoc.fugerit.org http://www.fugerit.org/data/java/doc/xsd/doc-1-4.xsd" > 	
+
+	<metadata> </metadata>
+	<body>
+					
+		<para>TITOLO 1</para>
+		
+		<para>TITOLO 2 <#if (false)>TEST 1
+					   		<#elseif (false)>TEST 2
+					   		<#elseif (true)>TEST 3
+					   		<#else>TEST 4
+					   </#if>
+		</para>
+		
+	</body>
+</doc>
---
+++ fj-doc-quickstart-b-freemarker/src/test/java/test/org/fugerit/java/doc/qs/QuickstartB006SimpleMarkdown.java
@@ -0,0 +1,61 @@
+package test.org.fugerit.java.doc.qs;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+import org.fugerit.java.core.xml.sax.SAXParseResult;
+import org.fugerit.java.doc.base.config.DocConfig;
+import org.fugerit.java.doc.base.facade.ProcessDocFacade;
+import org.fugerit.java.doc.base.process.DocProcessContext;
+import org.fugerit.java.doc.qs.data.sample.CharacterSample;
+import org.fugerit.java.doc.qs.facade.QuickstartDocFacade;
+import org.junit.Test;
+
+/**
+ * file: QuickstartB004ComplexProcessStep.java
+ * version : 001 (2021-02-24)
+ * 
+ * It is possible to use a more compact chain configuration
+ * 
+ * Goal : 	this example is very similar to QuickstartB003 but with a more compact chain configuration.
+ * 			
+ * 
+ * Maven dependancies : 
+ * org.fugerit.java/fj-core
+ * org.fugerit.java/fj-doc-base
+ * org.fugerit.java/fj-doc-freemarker	(for both html and pdf fop handlers)
+ * org.fugerit.java/fj-doc-mod-fop	(for pdf fop handler)
+ * 
+ * @see TestBase for common specs of the quickstart examples
+ * @see org.fugerit.java.doc.base.facade.ProcessDocFacade
+ * @see https://freemarker.apache.org/docs/ref_builtins.html for build in free marker functions
+ * 
+ * @author Matteo a.k.a. Fugerit <m@fugerit.org>
+ *
+ */
+public class QuickstartB006SimpleMarkdown extends TestBase {
+
+	/**
+	 * Name of the chain configured in out DocProcessConfig
+	 */
+	public static final String CHAIN_ID = "simple-markdown";
+
+	private void generateDoc( String type, String fileName ) {
+		File destFile = this.getOutputFile( fileName );
+		ProcessDocFacade docFacade = QuickstartDocFacade.getInstance();
+		DocProcessContext context = DocProcessContext.newContext( "characterList" , CharacterSample.getDataSample() );
+		try ( FileOutputStream dest = new FileOutputStream( destFile ) ) {
+			SAXParseResult result = docFacade.process( CHAIN_ID, type, context, dest, true );
+			logger.info( "dest -> {} (valid:{})", destFile.getCanonicalPath(), result.isPartialSuccess() );
+		} catch (Exception e) {
+			this.handleFailError( e );
+		}
+	}
+	
+	@Test
+	public void testMd() {
+		String type = DocConfig.TYPE_MD; // "md"
+		this.generateDoc( type , CHAIN_ID+"."+type );
+	}
+	
+}
