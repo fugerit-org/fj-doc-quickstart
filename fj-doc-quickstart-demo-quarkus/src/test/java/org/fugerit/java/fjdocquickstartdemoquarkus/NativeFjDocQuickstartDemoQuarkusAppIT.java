e4bfa9d6df84b55615b4de1b24ec11c11ad1492b
Matteo a.k.a. Fugerit <m@fugerit.org>
2022-11-01 15:43:02 +0100

    Updated pom version to 0.3.9.5
    Changed runtime version :
    * quarkus
    * springboog
    * websphere liberty

--- fj-doc-quickstart-demo-quarkus/src/test/java/org/fugerit/java/fjdocquickstartdemoquarkus/NativeFjDocQuickstartDemoQuarkusAppIT.java
+++
@@ -1,9 +0,0 @@
-package org.fugerit.java.fjdocquickstartdemoquarkus;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeFjDocQuickstartDemoQuarkusAppIT extends TestPdfServiceTest {
-
-    // Execute the same tests but in native mode.
-}
