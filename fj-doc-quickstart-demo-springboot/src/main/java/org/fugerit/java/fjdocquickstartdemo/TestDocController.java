1d7ebb06521c6d5059affd586b4f6ac41037f54d
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2023-08-16 11:13:40 +0200

    updated demo springboot

--- fj-doc-quickstart-demo-springboot/src/main/java/org/fugerit/java/fjdocquickstartdemo/TestDocController.java
+++ fj-doc-quickstart-demo-springboot/src/main/java/org/fugerit/java/fjdocquickstartdemo/TestDocController.java
@@ -6,37 +6,36 @@ import java.io.ByteArrayOutputStream;
 import javax.servlet.http.HttpServletResponse;
 
 import org.fugerit.java.doc.qs.doc.sample.FreemarkerDocSample;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 public class TestDocController {
-
-	private final static Logger logger = LoggerFactory.getLogger( TestDocController.class );
-
-	@RequestMapping(value = "/pdftest", method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_PDF_VALUE)
+	
+	@GetMapping( value = "/pdftest", produces = MediaType.APPLICATION_PDF_VALUE )
     public ResponseEntity<InputStreamResource> pdftest() {
 		try {
 	        var headers = new HttpHeaders();
 	        headers.add("Content-Disposition", "inline; filename=pdftest.pdf");
 	        ByteArrayOutputStream baos = new ByteArrayOutputStream();
 	        FreemarkerDocSample.generateCharacterListPdf( baos );
-	        ByteArrayInputStream bis = new ByteArrayInputStream( baos.toByteArray() );
+	        byte[] data = baos.toByteArray();
+	        ByteArrayInputStream bis = new ByteArrayInputStream( data );
+	        log.info( "test pdf OK : {}", data.length );
 	        return ResponseEntity
 	                .ok()
 	                .headers(headers)
 	                .contentType(MediaType.APPLICATION_PDF)
 	                .body(new InputStreamResource(bis));			
 		} catch (Exception e) {
-			logger.error( "Error : "+e, e );
+			log.error( "Error : "+e, e );
 	        return ResponseEntity
 	                .status( HttpServletResponse.SC_INTERNAL_SERVER_ERROR ).build();
 		}
