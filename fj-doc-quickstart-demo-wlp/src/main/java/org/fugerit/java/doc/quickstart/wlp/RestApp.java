7e8528b992869e1152d007689e300dd3b00c6e94
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2023-08-16 12:37:29 +0200

    Liberrty 23.0.0.7

--- fj-doc-quickstart-demo-wlp/src/main/java/org/fugerit/java/doc/quickstart/wlp/RestApp.java
+++ fj-doc-quickstart-demo-wlp/src/main/java/org/fugerit/java/doc/quickstart/wlp/RestApp.java
@@ -3,10 +3,10 @@ package org.fugerit.java.doc.quickstart.wlp;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.ws.rs.ApplicationPath;
+import jakarta.ws.rs.ApplicationPath;
 
 @ApplicationPath("/")
-public class RestApp extends javax.ws.rs.core.Application {
+public class RestApp extends jakarta.ws.rs.core.Application {
 
 	   public Set<Class<?>> getClasses() {
 		      Set<Class<?>> s = new HashSet<Class<?>>();
--- fj-doc-quickstart-demo-wlp/src/main/java/org/fugerit/java/doc/quickstart/wlp/TestPdfService.java
+++ fj-doc-quickstart-demo-wlp/src/main/java/org/fugerit/java/doc/quickstart/wlp/TestPdfService.java
@@ -2,32 +2,32 @@ package org.fugerit.java.doc.quickstart.wlp;
 
 import java.io.ByteArrayOutputStream;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Response;
-
 import org.fugerit.java.doc.qs.doc.sample.FreemarkerDocSample;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Response;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Path("/pdftest")
 public class TestPdfService {
 
-	private final static Logger logger = LoggerFactory.getLogger( TestPdfService.class );
-	
 	@GET
 	@Produces("application/pdf")
     public Response pdftest() {
 		try {
 			ByteArrayOutputStream baos = new ByteArrayOutputStream();
 			FreemarkerDocSample.generateCharacterListPdf( baos );
-			Response.ResponseBuilder responseBuilder = Response.ok(baos.toByteArray());
+			byte[] data = baos.toByteArray();
+			log.info( "test pdf OK : {}", data.length );
+			Response.ResponseBuilder responseBuilder = Response.ok(data);
 		    responseBuilder.type("application/pdf");
 		    responseBuilder.header("Content-Disposition", "inline; filename=pdftest.pdf");
 		    return responseBuilder.build();			
 		} catch (Exception e) {
-			logger.error( "Error : "+e, e );
+			log.error( "Error : "+e, e );
 	        return Response.status( 505 ).build();
 		}
     }
