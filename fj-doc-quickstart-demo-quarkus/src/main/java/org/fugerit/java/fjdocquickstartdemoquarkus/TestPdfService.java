d26c1d7d685222c892e1fd38cdc0f91e82cabb8f
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2023-08-16 11:40:09 +0200

    Quarkus demo updated

--- fj-doc-quickstart-demo-quarkus/src/main/java/org/fugerit/java/fjdocquickstartdemoquarkus/TestPdfService.java
+++ fj-doc-quickstart-demo-quarkus/src/main/java/org/fugerit/java/fjdocquickstartdemoquarkus/TestPdfService.java
@@ -8,26 +8,27 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
 import org.fugerit.java.doc.qs.doc.sample.FreemarkerDocSample;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Path("/pdftest")
 public class TestPdfService {
 
-	private final static Logger logger = LoggerFactory.getLogger( TestPdfService.class );
-	
 	@GET
 	@Produces("application/pdf")
     public Response pdftest() {
 		try {
 			ByteArrayOutputStream baos = new ByteArrayOutputStream();
 			FreemarkerDocSample.generateCharacterListPdf( baos );
-			Response.ResponseBuilder responseBuilder = Response.ok(baos.toByteArray());
+			byte[] data = baos.toByteArray();
+			log.info( "test pdf OK : {}", data.length );
+			Response.ResponseBuilder responseBuilder = Response.ok(data);
 		    responseBuilder.type("application/pdf");
 		    responseBuilder.header("Content-Disposition", "inline; filename=pdftest.pdf");
 		    return responseBuilder.build();			
 		} catch (Exception e) {
-			logger.error( "Error : "+e, e );
+			log.error( "Error : "+e, e );
 	        return Response.status( 505 ).build();
 		}
     }
